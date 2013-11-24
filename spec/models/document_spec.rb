60932b12256502386adc39b9f72879d25fe18111
redCashion <xperts@gmail.com>
2013-11-24 13:44:22 -0500

    simple tests pass

--- spec/models/document_spec.rb
+++ spec/models/document_spec.rb
@@ -2,9 +2,9 @@ require 'spec_helper'
 
 describe Document do
 
-  let(:basic) { Consumer::Document.new("#{Rails.root}/spec/sites/test/index.html") }
-  let(:chartbeat) { Consumer::Document.new("http://chartbeat.com/jobs") }
-  let(:google) { Consumer::Document.new("http://www.google.com") }
+  let(:basic) { Document.new("#{Rails.root}/spec/pages/craigslist/job_listing.html") }
+  let(:chartbeat) { Document.new("http://chartbeat.com/jobs") }
+  let(:google) { Document.new("http://www.google.com") }
 
   describe "#root" do
     it "should return a local page" do
@@ -16,9 +16,4 @@ describe Document do
     end
   end
 
-  describe "#children" do
-    it "should return the array of children" do
-      chartbeat.children.count.should > 0
-    end
-  end
 end
